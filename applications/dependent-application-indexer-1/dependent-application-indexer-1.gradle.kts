71a92ce8889c73bcd9a2b3ecb575832a179550c1
maartenvandeperre <maarten.vandeperre@viae.solutions>
2023-11-22 13:58:59 +0100

    add a camel example

--- applications/dependent-application-indexer-1/dependent-application-indexer-1.gradle.kts
+++ applications/dependent-application-indexer-1/dependent-application-indexer-1.gradle.kts
@@ -4,4 +4,19 @@ dependencies {
     implementation("io.quarkus:quarkus-hibernate-orm-panache")
     implementation("io.quarkus:quarkus-jdbc-postgresql")
     implementation("io.quarkus:quarkus-reactive-pg-client")
+
+    //Camel
+    implementation("org.apache.camel.quarkus:camel-quarkus-core")
+    implementation("org.apache.camel.quarkus:camel-quarkus-bean")
+    implementation("org.apache.camel.quarkus:camel-quarkus-jackson")
+    implementation("org.apache.camel.quarkus:camel-quarkus-kotlin")
+    implementation("org.apache.camel.quarkus:camel-quarkus-kotlin-dsl")
+    implementation("org.apache.camel.quarkus:camel-quarkus-rest")
+    implementation("org.apache.camel.quarkus:camel-quarkus-direct")
+    implementation("org.apache.camel.quarkus:camel-quarkus-http")
+    implementation("org.apache.camel.quarkus:camel-quarkus-timer")
+    implementation("org.apache.camel.quarkus:camel-quarkus-jdbc")
+    implementation("org.apache.camel.quarkus:camel-quarkus-log")
+    implementation("io.quarkus:quarkus-arc")
+    implementation("io.quarkus:quarkus-config-yaml")
 }
